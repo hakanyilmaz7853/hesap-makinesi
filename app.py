b916d88012056f825d7ef9bde8602ebb2954e641
Hakan YILMAZ <hakanyilmaz7853@gmail.com>
2025-10-06 11:04:14 +0300

    İlk commit

---
+++ app.py
@@ -0,0 +1,21 @@
+def topla(sayi1, sayi2):
+    sonuc = sayi1 + sayi2
+    return sonuc
+
+def cikar(sayi1, sayi2):
+    sonuc = sayi1 - sayi2
+    return sonuc
+
+while True:
+    sayi1 = int(input("Birinci sayı: "))
+    sayi2 = int(input("İkinci sayı: "))
+
+    islem = input("Toplama için 1, çıkarma için 2, çıkmak için 3'e basın")
+    if islem == "1":
+        print(topla(sayi1, sayi2))
+    elif islem == "2":
+        print(cikar(sayi1, sayi2))
+    elif islem == "3":
+        break
+    else:
+        print("Lütfen geçerli bir giriş yap")
